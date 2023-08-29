f41f6121fa8493f77398e594fe5e788f621eaf6d
zain <mzainkhan2005@gmail.com>
2023-08-29 18:24:32 +0500

    second commit

---
+++ file.js
@@ -0,0 +1,32 @@
+const catergories = document.querySelectorAll('.categorie');
+const productBuyItemButtons = document.querySelectorAll(
+  '.product-gallery-item'
+);
+let cardBadgeCount = 2;
+
+catergories.forEach((item) => {
+  item.querySelector('.categorie-name').addEventListener('click', () => {
+    if (item.classList.contains('active')) {
+      item.classList.remove('active');
+    } else {
+      closeAllCategroies();
+      item.classList.toggle('active');
+    }
+  });
+});
+
+productBuyItemButtons.forEach((button) => {
+  console.log(button);
+
+  button.addEventListener('click', () => {
+    console.log('click');
+    cardBadgeCount++;
+    console.log(cardBadgeCount);
+  });
+});
+
+function closeAllCategroies() {
+  catergories.forEach((item) => {
+    item.classList.remove('active');
+  });
+}
